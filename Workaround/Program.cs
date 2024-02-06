2ed3399ae5a5ec5fca1bade88b1aab28b49626c5
Erencan Bilge <82596801+ErenEfendi@users.noreply.github.com>
2024-02-06 16:46:44 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,61 @@
+﻿using Entities.Concrete;
+using Business.Concrete;
+
+using System.IO.Pipes;
+using System.Reflection.Metadata.Ecma335;
+using Business.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            SelamVer("Erencan");
+            SelamVer("Ubeydullah");
+            SelamVer("Abdulrezak");
+
+            int sonuc = Topla(22,9);
+
+            string ogrenci1 = "Erencan";
+            string ogrenci2 = "Ubeydullah";
+            string ogrenci3 = "Abdulrezak";
+
+            string[] ogrenciler = new string[3];
+            ogrenciler[0] = ogrenci1;
+            ogrenciler[1] = ogrenci2;
+            ogrenciler[2] = ogrenci3;
+
+            for (int i = 0; i < ogrenciler.Length; i++)
+            {
+                Console.WriteLine(ogrenciler[i]);
+            }
+
+            Person person1 = new Person();
+            person1.FirstName = "Eren Can";
+            person1.LastName = "Bilge";
+            person1.DateOfBirthYear = 2002;
+            person1.NationalIdentity = 35332606064;
+
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+        static void SelamVer(string isim="isimsiz") // default yapmak için 
+        {
+            Console.WriteLine("Merhaba "+ isim);
+        }
+
+        static int Topla(int sayi1, int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam: "+sonuc.ToString());
+            return sonuc;
+        }
+
+       
+    }
+}
